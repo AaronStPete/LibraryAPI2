16088436be2540bc43c464333206a519c66d8df9
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-23 15:12:09 -0400

    Add project files.

---
+++ LibraryAPI2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LibraryAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LibraryAPI2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibraryAPI2.WebApiApplication" Language="C#" %>
