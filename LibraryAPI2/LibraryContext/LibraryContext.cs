578b8031dfca82e46ef12d729fec546e58221bc6
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-26 16:19:31 -0400

    Created CheckoutLedger model and added to LibraryContext

--- LibraryAPI2/LibraryContext/LibraryContext.cs
+++ LibraryAPI2/LibraryContext/LibraryContext.cs
@@ -17,6 +17,6 @@ namespace LibraryAPI2.LibraryContext
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
+        public DbSet<CheckoutLedger> CheckoutLedgers { get; set; }
     }
 }
---
+++ LibraryAPI2/Models/CheckoutLedger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAPI2.Models
+{
+    public class CheckoutLedger
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+        public string Email { get; set; } = "";
+
+        //Foreign Key
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
