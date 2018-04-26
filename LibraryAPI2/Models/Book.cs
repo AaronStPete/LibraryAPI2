48129f8b89216337c3c2f00b660a490fd110ff16
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-26 15:40:23 -0400

    Added LibraryContext; added DBSetBook; Fixed Models.Book's namespace

---
+++ LibraryAPI2/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAPI2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string YearPublished { get; set; }
+        public string Condition { get; set; }
+        public bool IsCheckedOut { get; set; } = false;
+        public DateTime DueBackDate { get; set; } = DateTime.Now.AddDays(10);
+
+        //Foreign Keys:
+
+        //public int AuthorId { get; set; }
+        //
+
+        //public int GenreId { get; set; }
+
+    }
+}
